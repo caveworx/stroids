b0f77a630b76188d6f4d41f0387483533c111ba0
caveworx <dblumley51@gmail.com>
2024-08-27 17:58:39 -0700

    frame delay

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    timer = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0, 0, 0))
+        screen.fill((0, 0, 0))
         pygame.display.flip
+        dt = timer.tick(60) / 1000
         
 
 
