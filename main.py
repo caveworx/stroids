1e239d86bf90c16ddd8486fb108778886865b9dd
caveworx <dblumley51@gmail.com>
2024-08-29 19:55:17 -0700

    movement!

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from circleshape import *
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     timer = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -15,9 +15,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(('white'))
+            
+        player.update(dt)
+        screen.fill("black")
         player.draw(screen)
-        pygame.display.flip
+        
+        pygame.display.flip()
         dt = timer.tick(60) / 1000
         
 
