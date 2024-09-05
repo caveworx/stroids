210293b0574fd2cba4db0aeb2aa1b82c6f2df092
caveworx <dblumley51@gmail.com>
2024-09-04 17:31:56 -0700

    moving circles

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import *
 from player import *
 from circleshape import *
+from asteroid import *
+from asteroidfield import *
 
 
 def main():
@@ -9,10 +11,19 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     timer = pygame.time.Clock()
     dt = 0
+
+    #groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    #grouped classes
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
 
 
     while True:
@@ -22,11 +33,14 @@ def main():
             
         for item in updatable:
             item.update(dt)
+
         screen.fill("black")
+
         for item in drawable:
             item.draw(screen)
         
         pygame.display.flip()
+        
         dt = timer.tick(60) / 1000
         
 
